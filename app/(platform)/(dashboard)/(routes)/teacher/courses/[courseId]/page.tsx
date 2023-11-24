cb0c0e88fd446d84f7db85430164100d22835afb
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-24 13:22:01 +0700

    Done config uploadthing + image-form in teacher course

--- app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,9 +1,13 @@
-import { IconBadge } from '@/components/icon-badge';
-import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
-import { LayoutDashboard } from 'lucide-react';
 import { redirect } from 'next/navigation';
+import { LayoutDashboard } from 'lucide-react';
+
+import { db } from '@/lib/db';
+import { IconBadge } from '@/components/icon-badge';
+
 import { TitleForm } from './_components/title-form';
+import { DescriptionForm } from './_components/description-form';
+import { ImageForm } from './_components/image-form';
 
 const CourseIdPage = async ({
 	params,
@@ -38,6 +42,7 @@ const CourseIdPage = async ({
 	const completedFields = requiredFields.filter(Boolean).length;
 
 	const completionText = `(${completedFields}/${totalFields})`;
+
 	return (
 		<div className='p-6'>
 			<div className='flex items-center justify-between'>
@@ -48,12 +53,19 @@ const CourseIdPage = async ({
 					</span>
 				</div>
 			</div>
-			<div className='grid grid-cols-1 md:grid-cols-2 first-line:gap-6 mt-16'>
-				<div className='flex items-center gap-x-2'>
-					<IconBadge icon={LayoutDashboard} />
-					<h2 className='text-xl'>Customize your course</h2>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+				<div>
+					<div className='flex items-center gap-x-2'>
+						<IconBadge icon={LayoutDashboard} />
+						<h2 className='text-xl'>Customize your course</h2>
+					</div>
+					<TitleForm initialData={course} courseId={course.id} />
+					<DescriptionForm
+						initialData={course}
+						courseId={course.id}
+					/>
+					<ImageForm initialData={course} courseId={course.id} />
 				</div>
-				<TitleForm initialData={course} courseId={course.id} />
 			</div>
 		</div>
 	);
--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({publicRoutes: ['/landing']});
+export default authMiddleware({publicRoutes: ['/landing', '/api/uploadthing']});
  
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  
 };
  
