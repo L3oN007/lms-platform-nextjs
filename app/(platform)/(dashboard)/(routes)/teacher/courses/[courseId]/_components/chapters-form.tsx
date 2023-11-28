067bc2d3114fef567577fc556f4ac4fcc78559a1
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-28 14:29:00 +0700

    Done chapter description with react-quill in teacher course

--- app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -76,6 +76,10 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   };
 
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
+
   return (
     <div className="relative mt-6 rounded-md border bg-slate-100 p-4">
       {isUpdating && (
@@ -139,7 +143,7 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
         >
           {!initialData.chapters.length && "No chapters"}
           <ChaptersList
-            onEdit={() => {}}
+            onEdit={onEdit}
             onReorder={onReorder}
             items={initialData.chapters || []}
           />
